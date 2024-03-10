a014ab9f4688462932b861751e6b614e69b343c9
xuerongNanopay <xuerong@nanopay.net>
2024-03-10 01:43:46 -0500

    add struct for Block and Block Chain

--- src/block.rs
+++ src/block.rs
@@ -1,1 +1,16 @@
-// pub 
+pub struct Block {
+    timestamp: u128, //The time when the block is created.
+    transactions: String, //TODO: string as placeholder
+    prev_block_hash: String,
+    hash: String,
+    height: usize,
+    nonce: i32, //For difficulty in Proof of Work
+}
+
+pub struct BlockChain {
+    blocks: Vec<Block>
+}
+
+// impl Block {
+//     pub fn new(data: String, )
+// }
