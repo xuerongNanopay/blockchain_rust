f7ae0b1b0765f5351a9dc05505ad3e1959159170
xuerongNanopay <xuerong@nanopay.net>
2024-03-11 00:47:14 -0400

    add anyhow to handle generic error

--- src/block.rs
+++ src/block.rs
@@ -1,3 +1,6 @@
+use std::time::{SystemTime};
+use anyhow::Result;
+
 pub struct Block {
     timestamp: u128, //The time when the block is created.
     transactions: String, //TODO: string as placeholder
@@ -11,6 +14,21 @@ pub struct BlockChain {
     blocks: Vec<Block>
 }
 
-// impl Block {
-//     pub fn new(data: String, )
-// }
+impl Block {
+    pub fn new(data: String, prev_block_hash: String, height: usize) -> Result<Block> {
+        // let timestamp:u128 = System
+        let timestamp: u128 = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)?
+            .as_millis();
+        
+        let mut block = Block {
+            timestamp,
+            transactions: data,
+            prev_block_hash,
+            hash: String::new(),
+            height,
+            nonce: 0,
+        };
+        Ok(block)
+    }
+}
