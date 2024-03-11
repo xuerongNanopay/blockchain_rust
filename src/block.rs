0a4c8e4fee6274a5d037f3500354b4604524c600
xuerongNanopay <xuerong@nanopay.net>
2024-03-11 11:11:39 -0400

    validate fun finish

--- src/block.rs
+++ src/block.rs
@@ -3,6 +3,8 @@ use anyhow::Result;
 use log::info;
 use sha2::{Sha256, Digest};
 
+const TARGET_HEXT: usize = 4;
+
 pub struct Block {
     timestamp: u128, //The time when the block is created.
     transactions: String, //TODO: string as placeholder
@@ -50,11 +52,21 @@ impl Block {
     }
 
     fn validate(&self) -> Result<bool> {
-        Ok(true)
+        let data: Vec<u8> = self.prepare_hash_data()?;
+        let mut hasher: Sha256 = Sha256::new();
+        hasher.update(&data[..]);
+
+        // Dummy PoW
+        let mut vec1: Vec<u8> = vec![];
+        vec1.resize(TARGET_HEXT, '0' as u8);
+        println!("{:?}", vec1);
+
+        //Compare hash.
+        Ok(&hasher.finalize()[0..TARGET_HEXT] == &vec1[0..TARGET_HEXT])
     }
 
     // Decide the properties that needs include in the hash.
-    fn prepare_hash_data(&mut self) -> Result<Vec<u8>> {
+    fn prepare_hash_data(&self) -> Result<Vec<u8>> {
         let content = (
             self.prev_block_hash.clone(),
             self.transactions.clone(),
