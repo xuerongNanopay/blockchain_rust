f473180372c03bfe64930bbe1f13cb8eb9fc9c62
xuerongNanopay <xuerong@nanopay.net>
2024-03-13 12:45:21 -0400

    add model for input and output

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod block;
 mod errors;
 mod blockchain;
 mod cli;
+mod transaction;
 
 use errors::Result;
 use cli::Cli;
@@ -9,6 +10,6 @@ use cli::Cli;
 fn main() -> Result<()> {
     let mut cli = Cli::new()?;
     cli.run()?;
-    
+
     Ok(())
 }
---
+++ src/transaction.rs
@@ -0,0 +1,22 @@
+
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Transaction {
+    pub id: String,
+    pub vin: Vec<TXInput>,
+    pub vout: Vec<TXOutput>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct TXInput {
+    pub txid: String,
+    pub vout: i32,
+    pub script_sig: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct TXOutput {
+    pub value: i32,
+    pub script_pub_key: String
+}
